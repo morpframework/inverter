fe03ebf3b2fbb743d245d9e7d50522a94c91d905
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-11-12 17:12:46 +0800

    added license file

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1.0'
+
+def readfile(name):
+    with open(os.path.join(os.path.dirname(__file__), name)) as f:
+        out = f.read()
+    return out
+
+desc = '\n'.join([readfile('README.rst'), readfile('CHANGELOG.rst')])
 
 setup(name='inverter',
       version=version,
       description="Convert dataclass to other schemas",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=desc,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='dataclass converter',
       author='Izhar Firdaus',
       author_email='kagesenshi.87@gmail.com',
